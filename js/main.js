8deaf3ffb9d74b582c40b6f594b80b8566cde99f
Xolmuxammad <iprogerr08@gmail.com>
2022-11-05 16:45:26 +0500

    repeat tabs code again

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
 const parentBlock = document.querySelector('.container'),
-      tabContents = document.querySelectorAll('.show_item'),
-      tabs = document.querySelectorAll('.tabs');
+  tabContents = document.querySelectorAll('.item'),
+  tabsParent = document.querySelector('.tabs'),
+  tabs = document.querySelectorAll('.tabheader__item');
+function hideTabsContent() {
+  tabContents.forEach(item => {
+    item.classList.add('hide');
+    item.classList.remove('show');
+  });
+
+  tabs.forEach(tab => {
+    tab.classList.remove('back');
+  });
+}
+function showTabsContent(i = 0) {
+  tabContents[i].classList.add('show');
+  tabContents[i].classList.remove('hide');
+
+  tabs[i].classList.add('back');
+}
+hideTabsContent();
+showTabsContent();
+tabsParent.addEventListener('click', e => {
+  let targetEl = e.target;
+
+  if (targetEl && targetEl.classList.contains('tabheader__item'))
+    tabs.forEach((item, i) => {
+      if (targetEl == item) {
+        hideTabsContent();
+        showTabsContent(i);
+      }
+    });
+});
