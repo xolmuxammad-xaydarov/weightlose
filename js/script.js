c85305bd5b010bc66a2970aaeaafe79710fd7ac3
Xolmuxammad <iprogerr08@gmail.com>
2022-11-24 15:32:31 +0500

    solve a problem with class

--- js/script.js
+++ js/script.js
@@ -41,44 +41,44 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Timer
 
-  const deadline = '2022-11-22';
+  const deadline = '2022-12-22';
 
   function getTimeRemain(endtime) {
 
     const t = Date.parse(endtime) - Date.parse(new Date()),
-          days = Math.floor((t / (1000 * 60 * 60 * 24))),
-          hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-          minutes = Math.floor((t / 1000 / 60) % 60),
-          seconds = Math.floor((t / 1000) % 60);
+      days = Math.floor((t / (1000 * 60 * 60 * 24))),
+      hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+      minutes = Math.floor((t / 1000 / 60) % 60),
+      seconds = Math.floor((t / 1000) % 60);
 
-    return{
-          'total':t,
-          'days':days,
-          'hours':hours,
-          'minutes':minutes,
-          'seconds':seconds
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
     }
   }
 
-  function getZero(num){
-    if(num >= 0 && num < 10){
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
       return `0${num}`;
-    }else{
+    } else {
       return num;
     }
   }
 
-  function setClock (selector, endtime){
+  function setClock(selector, endtime) {
     let timer = document.querySelector(selector),
-        days = timer.querySelector('#days'),
-        hours = timer.querySelector('#hours'),
-        minutes = timer.querySelector('#minutes'),
-        seconds = timer.querySelector('#seconds'),
-        timeInterval = setInterval(update, 1000);
+      days = timer.querySelector('#days'),
+      hours = timer.querySelector('#hours'),
+      minutes = timer.querySelector('#minutes'),
+      seconds = timer.querySelector('#seconds'),
+      timeInterval = setInterval(update, 1000);
 
     update();
 
-    function update(){
+    function update() {
       let t = getTimeRemain(endtime);
 
       days.innerHTML = getZero(t.days);
@@ -86,7 +86,7 @@ window.addEventListener('DOMContentLoaded', () => {
       minutes.innerHTML = getZero(t.minutes);
       seconds.innerHTML = getZero(t.seconds);
 
-      if(t.total <= 0){
+      if (t.total <= 0) {
         clearInterval(timeInterval);
       }
     }
@@ -94,4 +94,66 @@ window.addEventListener('DOMContentLoaded', () => {
 
   setClock('.timer', deadline);
 
+  // Cards
+
+class MenuCard{
+  constructor(src, alt, title, desrc, price, parentElem){
+    this.src = src
+    this.alt = alt
+    this.title = title
+    this.desrc = desrc
+    this.price = price
+    this.transfer = 37
+    this.changeToUAH()
+    this.parentElem = document.querySelector(parentElem)
+  }
+
+  changeToUAH(){
+    this.price = this.price * this.transfer;
+  }
+
+  render(){
+    const element = document.createElement('div');
+    element.innerHTML = `
+      <div class="menu__item">
+        <img src=${this.src} alt=${this.alt}>
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr">${this.desrc}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+        </div>
+      </div>`
+
+      this.parentElem.append(element);   
+    }
+  }
+
+  new MenuCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежиховощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальнойценой и высоким качеством!',
+    9,
+    ".menu .container"
+  ).render();
+
+  new MenuCard(
+    "img/tabs/elite.jpg",
+    "elite",
+    'Меню “Премиум”',
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, нои качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без походав ресторан!',
+    14,
+    ".menu .container"
+  ).render();
+
+  new MenuCard(
+    "img/tabs/post.jpg",
+    "post",
+    'Меню "Постное"',
+    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    21,
+    ".menu .container"
+  ).render();
 });
